04ff234ff651c584401f791b32cda1bcc56d0b39
Angel Stoynov <an.stoynov@gmail.com>
2025-11-19 22:12:34 +0200

    Display all possible solutions

--- container/src/main/java/com/uni/angel/container/ContainerResolver.java
+++ container/src/main/java/com/uni/angel/container/ContainerResolver.java
@@ -2,6 +2,7 @@ package com.uni.angel.container;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
@@ -11,53 +12,75 @@ public class ContainerResolver {
         throw new UnsupportedOperationException("Cannot instantiate");
     }
 
-    public static int solve(List<Container> containers, int weightCapacity) {
-        int[] bestValue = new int[weightCapacity + 1];
-        int[] choice = new int[weightCapacity + 1];
+    public static int solve(List<Container> containers, int capacity) {
+        int n = containers.size();
+        int[] bestValue = new int[capacity + 1];
 
-        for (int capacity = 0; capacity <= weightCapacity; capacity++) {
-            for (int i = 0; i < containers.size(); i++) {
-                Container container = containers.get(i);
+        for (int c = 0; c <= capacity; c++) {
+            for (Container container : containers) {
+                int w = container.weight();
+                int v = container.value();
 
-                int weight = container.weight();
-                int value = container.value();
-
-                if (weight > capacity) {
-                    log.debug("Container (weight={}, value={}) does NOT fit into capacity {}", weight, value, capacity);
-                    continue;
-                }
-
-                int previous = bestValue[capacity];
-                int candidate = value + bestValue[capacity - weight];
-
-                log.debug(
-                        "Try container {} (weight={}, value={}): candidate = {} + bestValue[{}] = {}",
-                        i, weight, value, value, (capacity - weight), candidate
-                );
-
-                if (candidate > previous) {
-                    bestValue[capacity] = candidate;
-                    choice[capacity] = i;
-
-                    log.info("UPDATE: bestValue[{}] changed {} → {} using container i={}, (weight={}, value={})",
-                            capacity, previous, candidate, i, weight, value);
+                if (w <= c) {
+                    bestValue[c] = Math.max(bestValue[c], v + bestValue[c - w]);
                 }
             }
         }
 
-        int[] count = new int[containers.size()];
-        int remaining = weightCapacity;
+        int optimalValue = bestValue[capacity];
+        log.info("Optimal value = {}", optimalValue);
 
-        while (remaining > 0) {
-            int idx = choice[remaining];
-            count[idx]++;
-            remaining -= containers.get(idx).weight();
+        List<int[]> solutions = new ArrayList<>();
+        dfsCombinations(
+                containers, capacity, optimalValue,
+                0,
+                new int[n],
+                solutions
+        );
+
+        for (int[] sol : solutions) {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < sol.length; i++) {
+                sb.append("x").append(i + 1).append(" = ").append(sol[i]).append(" ");
+            }
+
+            log.info(sb.toString());
         }
 
-        for (int i = 0; i < containers.size(); i++) {
-            log.info("x{} = {}", i + 1, count[i]);
+        return optimalValue;
+    }
+
+    private static void dfsCombinations(
+            List<Container> containers,
+            int remainingWeight,
+            int remainingValue,
+            int startIndex,
+            int[] count,
+            List<int[]> out
+    ) {
+        if (remainingWeight == 0 && remainingValue == 0) {
+            out.add(count.clone());
+            return;
         }
 
-        return bestValue[weightCapacity];
+        if (remainingWeight < 0 || remainingValue < 0)
+            return;
+
+        for (int i = startIndex; i < containers.size(); i++) {
+            int w = containers.get(i).weight();
+            int v = containers.get(i).value();
+
+            count[i]++;
+            dfsCombinations(
+                    containers,
+                    remainingWeight - w,
+                    remainingValue - v,
+                    i,
+                    count,
+                    out
+            );
+            count[i]--;
+        }
     }
 }
